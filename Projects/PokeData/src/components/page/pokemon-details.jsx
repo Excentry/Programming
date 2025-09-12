bcb69532a0263819cd45250f42465d4f4ed1338e
Excentry <feitopitufi3.2@gmail.com>
2025-09-12 12:16:35 -0500

    Change in the eslint

--- Projects/PokeData/src/components/page/pokemon-details.jsx
+++ Projects/PokeData/src/components/page/pokemon-details.jsx
@@ -11,7 +11,13 @@ export function PokemonDetails() {
 
   useFetchCardPokemon(nombre, setPokemon)
 
-  const { id_pokemon, ratio_captura, habilidad, tipos: tipo, categoria } = pokemon
+  const {
+    id_pokemon,
+    ratio_captura,
+    habilidad,
+    tipos: tipo,
+    categoria,
+  } = pokemon
 
   const { ratioColor, habilityColor, typeColor, categoryColor } = useInfoColor(
     ratio_captura,
@@ -21,52 +27,60 @@ export function PokemonDetails() {
   )
 
   return (
-    <div className='pokemon-details'>
+    <section>
       <Link to='/' className='return-to-cards pokemon-card-link'>
         <FaArrowAltCircleLeft />
         <span> VOLVER </span>
       </Link>
-      <h2
-        className='pokemon-name-details'
-        style={{ background: habilityColor }}
-      >
-        {nombre}
-      </h2>
-      <img
-        src={getPokemonImg(id_pokemon)}
-        alt={nombre}
-        type='image/png'
-        className='pokemon-details-image'
-      />
-      <span
-        className='ratio-captura-details'
-        style={{ background: ratioColor }}
-      >
-        <h4>Ratio de captura</h4>
-        {ratio_captura}
-      </span>
-      <span className='habilidad-details' style={{ background: habilityColor }}>
-        <h4>Habilidad</h4>
-        {habilidad}
-      </span>
-      <span className='tipo-details' style={{ background: 'gray' }}>
-        <h4 className='tipo-details-title'>Tipo</h4>
-        {tipo?.split(',').map((type, i) => (
-          <span
-            key={i}
-            style={{
-              backgroundColor: typeColor[i],
-            }}
-            className='tipo-details-item'
-          >
-            {type.trim()}
-          </span>
-        ))}
-      </span>
-      <span className='categoria-details' style={{ background: categoryColor }}>
-        <h4>Especie</h4>
-        {categoria}
-      </span>
-    </div>
+      <div className='pokemon-details'>
+        <h2
+          className='pokemon-name-details'
+          style={{ background: habilityColor }}
+        >
+          {nombre}
+        </h2>
+        <img
+          src={getPokemonImg(id_pokemon)}
+          alt={nombre}
+          type='image/png'
+          className='pokemon-details-image'
+        />
+        <span
+          className='ratio-captura-details'
+          style={{ background: ratioColor }}
+        >
+          <h4>Ratio de captura</h4>
+          {ratio_captura}
+        </span>
+        <span
+          className='habilidad-details'
+          style={{ background: habilityColor }}
+        >
+          <h4>Habilidad</h4>
+          {habilidad}
+        </span>
+        <span className='tipo-details' style={{ background: 'gray' }}>
+          <h4 className='tipo-details-title'>Tipo</h4>
+          {tipo?.split(',').map((type, i) => (
+            <span
+              key={i}
+              style={{
+                backgroundColor: typeColor[i],
+              }}
+              className='tipo-details-item'
+            >
+              {type.trim()}
+            </span>
+          ))}
+        </span>
+        <span
+          className='categoria-details'
+          style={{ background: categoryColor }}
+        >
+          <h4>Especie</h4>
+          {categoria}
+        </span>
+      </div>
+    </section>
   )
 }
