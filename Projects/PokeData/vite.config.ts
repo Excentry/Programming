d3c7853d14208c2cd48e8a5a769b4d7b69a6cada
Excentry <feitopitufi3.2@gmail.com>
2025-09-23 01:26:03 -0500

    Add alias resolution in vite.config.ts

--- Projects/PokeData/vite.config.ts
+++ Projects/PokeData/vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@App': path.resolve(__dirname, 'src/App'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@images': path.resolve(__dirname, 'src/images'),
+      '@lib': path.resolve(__dirname, 'src/lib'),
+      '@logic': path.resolve(__dirname, 'src/logic'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@types': path.resolve(__dirname, 'src/types'),
+    },
   },
 })
