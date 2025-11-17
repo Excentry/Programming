3a1b775c23da6130e2da7637559eb2ee23313986
Excentry <feitopitufi3.2@gmail.com>
2025-11-17 01:21:10 -0500

    [FEAT]: Import media-screen.css and prettie root ReactDOM

--- Projects/PokeData/src/main.tsx
+++ Projects/PokeData/src/main.tsx
@@ -1,11 +1,14 @@
 import '@styles/index.css'
 import '@styles/App.css'
+import '@styles/media-screens.css'
 
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import { App } from '@App'
 
-const root = ReactDOM.createRoot(document.getElementById('root')!)
+const root = ReactDOM.createRoot(
+  document.getElementById('root')!
+)
 
 root.render(
   <BrowserRouter>
